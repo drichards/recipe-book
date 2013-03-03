ec775409ad7348d73fe7ece5fff238bc642a2dfc
David Richards <drichards@zipcar.com>
2013-03-03 13:18:10 -0500

    inital commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.2'
+
+gem 'sqlite3'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :test, :development do
+    gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecipeBook::Application.config.secret_token = '284512ed9b2f95709c2d25e0a2b1116359933e86ba05ca88649438562bd5b0aaed565cd913d020ca28a65c8b1fc9206f1b36937234148e59b9b1acf9462fc6d5'
