a949a0633fee566d443993fedfc251a9ddc7ea6d
David Richards <drichards@zipcar.com>
2013-03-03 22:33:14 -0500

    add categories to recipes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130303202246) do
+ActiveRecord::Schema.define(:version => 20130304033140) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "ingredients", :force => true do |t|
     t.string   "description"
@@ -22,9 +29,10 @@ ActiveRecord::Schema.define(:version => 20130303202246) do
 
   create_table "recipes", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.string   "process"
+    t.integer  "category_id"
   end
 
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Recipe do
   it { should have_many(:ingredients) }
+  it { should belong_to(:category) }
   
   it "should be valid with some ingredients" do
     create(:recipe).should be_valid
